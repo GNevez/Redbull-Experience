98427ae8f59887d6222a8c84d8c3e0c26e1cb9fa
Guilherme Neves <guilhermemferraz@hotmail.com>
2026-07-02 00:19:00 -0300

    feat: studio lighting with silver rims and subtle red accent

--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -5,63 +5,49 @@ import { Environment, Lightformer } from "@react-three/drei";
 export default function Lights() {
   return (
     <>
-      <ambientLight intensity={1.05} color="#ffffff" />
+      <ambientLight intensity={0.25} color="#dfe8ff" />
 
-      <directionalLight
-        position={[3, 5, 6]}
-        intensity={1.7}
-        color="#fffaf2"
-      />
-
-      <directionalLight
-        position={[-5, 2, 4]}
-        intensity={1.3}
-        color="#eef2ff"
-      />
-
-      <directionalLight
-        position={[0, -2, 5]}
-        intensity={0.9}
-        color="#ffffff"
-      />
+      <directionalLight position={[4, 6, 5]} intensity={1.2} color="#f5ecdc" />
 
       <Environment resolution={256} background={false}>
         <Lightformer
           form="rect"
-          intensity={3.2}
-          color="#ffffff"
-          position={[0, 4, 5]}
-          scale={[16, 8, 1]}
+          intensity={4}
+          color="#dce6f5"
+          position={[-6, 1, 2]}
+          rotation={[0, Math.PI / 2.6, 0]}
+          scale={[1.5, 10, 1]}
         />
         <Lightformer
           form="rect"
-          intensity={2.6}
-          color="#f2f5ff"
-          position={[-7, 1, 3]}
-          scale={[8, 12, 1]}
+          intensity={4}
+          color="#e8eef8"
+          position={[6, 1, 2]}
+          rotation={[0, -Math.PI / 2.6, 0]}
+          scale={[1.5, 10, 1]}
+        />
+        <Lightformer
+          form="rect"
+          intensity={2.2}
+          color="#ffffff"
+          position={[0, 6, 3]}
+          rotation={[-Math.PI / 2.4, 0, 0]}
+          scale={[10, 4, 1]}
+        />
+        <Lightformer
+          form="rect"
+          intensity={0.8}
+          color="#aebdd8"
+          position={[0, -5, 4]}
+          scale={[10, 3, 1]}
+        />
+        <Lightformer
+          form="rect"
+          intensity={0.3}
+          color="#ff2a2a"
+          position={[-8, -1, -2]}
           rotation={[0, Math.PI / 3, 0]}
-        />
-        <Lightformer
-          form="rect"
-          intensity={2.6}
-          color="#fff5ea"
-          position={[7, 2, 3]}
-          scale={[8, 12, 1]}
-          rotation={[0, -Math.PI / 3, 0]}
-        />
-        <Lightformer
-          form="rect"
-          intensity={1.6}
-          color="#ffffff"
-          position={[0, -4, 4]}
-          scale={[12, 6, 1]}
-        />
-        <Lightformer
-          form="ring"
-          intensity={2.4}
-          color="#ffffff"
-          position={[0, 2, 7]}
-          scale={[5, 5, 1]}
+          scale={[2, 6, 1]}
         />
       </Environment>
     </>
