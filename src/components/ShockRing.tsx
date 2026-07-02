a9a63821708b7c9b598ee5ca25ef3efdd532141a
Guilherme Neves <guilhermemferraz@hotmail.com>
2026-07-02 00:21:00 -0300

    feat: add impact shock ring component

---
+++ src/components/ShockRing.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { forwardRef } from "react";
+import * as THREE from "three";
+
+const ShockRing = forwardRef<THREE.Mesh>(function ShockRing(_props, ref) {
+  return (
+    <mesh
+      ref={ref}
+      position={[0, -3.38, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      scale={0.2}
+    >
+      <ringGeometry args={[0.92, 1, 64]} />
+      <meshBasicMaterial
+        color="#c8d4e6"
+        transparent
+        opacity={0}
+        toneMapped={false}
+        depthWrite={false}
+      />
+    </mesh>
+  );
+});
+
+export default ShockRing;
