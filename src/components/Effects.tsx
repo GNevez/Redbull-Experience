9cdb25d662b3152e86f26b6791029b0a9b59953d
Guilherme Neves <guilhermemferraz@hotmail.com>
2026-07-02 00:20:04 -0300

    feat: retuned depth of field with vignette and film grain

--- src/components/Effects.tsx
+++ src/components/Effects.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  DepthOfField,
+  Vignette,
+  Noise,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
 
 export default function Effects() {
   return (
     <EffectComposer multisampling={4}>
       <DepthOfField
         worldFocusDistance={6}
-        worldFocusRange={6}
-        focalLength={0.018}
-        bokehScale={3.5}
+        worldFocusRange={5}
+        focalLength={0.02}
+        bokehScale={2.5}
         height={700}
       />
+      <Vignette offset={0.3} darkness={0.55} />
+      <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} opacity={0.04} />
     </EffectComposer>
   );
 }
